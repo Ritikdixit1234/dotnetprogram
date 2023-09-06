8ab215f35b9cc1976c91fffc6b0ead92df79b105
Ritikdixit1234 <143371349+Ritikdixit1234@users.noreply.github.com>
2023-09-06 09:07:46 +0530

    Add files via upload

---
+++ ass2_q10.cs
@@ -0,0 +1,66 @@
+﻿
+//Question-10
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        sealed class Bank
+        {
+            public int AccountNumber;
+            public string name;
+            public double balance;
+
+            public Bank(int AccountNumber, string name, double balance)
+            {
+                this.AccountNumber = AccountNumber;
+                this.name = name;
+                this.balance = balance;
+            }
+            public void deposit(double balance)
+            {
+                balance += balance;
+            }
+            public void withdrawl(double amount)
+            {
+                if (amount > balance)
+                {
+                    Console.WriteLine("Insufficinent Balance");
+                }
+                else
+                {
+                    balance = balance - amount;
+                }
+            }
+            public void displayData()
+            {
+                Console.WriteLine("AccountNumber:" + this.AccountNumber);
+                Console.WriteLine("Name :" + this.name);
+                Console.WriteLine("Balance :" + this.balance);
+                Console.ReadKey();
+            }
+        }
+        class SavingAccount : Bank
+        {
+            public int AccountNumber;
+            public string name;
+
+        }
+    }
+
+}
+
