5adc740818b8b31f7fc7eefde03dfe7abb94bd66
Ritikdixit1234 <143371349+Ritikdixit1234@users.noreply.github.com>
2023-09-06 09:01:31 +0530

    Add files via upload

---
+++ ass2_q6.cs
@@ -0,0 +1,68 @@
+﻿//Question-6
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public partial class Employee
+    {
+        public int id;
+        public string name;
+        public double salary;
+
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+    }
+}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public partial class Employee
+    {
+
+        public void displayData()
+        {
+            Console.WriteLine("id :" + this.id);
+            Console.WriteLine("Name :" + this.name);
+            Console.WriteLine("Salary :" + this.salary);
+            Console.ReadKey();
+        }
+    }
+}
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(77, "Ritik dixit", 10000);
+            e1.displayData();
+        }
+
+    }
+
+}
