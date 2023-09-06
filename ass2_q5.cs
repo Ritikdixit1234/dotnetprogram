b734a81e9163e54d99660c0e02d309b9602d8004
Ritikdixit1234 <143371349+Ritikdixit1234@users.noreply.github.com>
2023-09-06 08:59:35 +0530

    Add files via upload

---
+++ ass2_q5.cs
@@ -0,0 +1,65 @@
+﻿//Question-5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public partial class Person
+    {
+        public string firstName;
+        public string lastName;
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+    }
+}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public partial class Person
+    {
+        public void showDetails()
+        {
+
+            Console.WriteLine("FirstName :" + firstName + "LastName :" + lastName);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("ritik", "dixit");
+            p1.showDetails();
+
+        }
+
+    }
+
+}
