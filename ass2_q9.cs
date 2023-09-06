419d8e2f7bfaf47674dc7b091332f7475fb39503
Ritikdixit1234 <143371349+Ritikdixit1234@users.noreply.github.com>
2023-09-06 09:06:18 +0530

    Add files via upload

---
+++ ass2_q9.cs
@@ -0,0 +1,47 @@
+﻿//Question-9
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Audi = new Car("bmw");
+
+        }
+
+    }
+
+    public sealed class Vechile
+    {
+        string vechile;
+        public void startEngine(string vechile)
+        {
+            this.vechile = vechile;
+            Console.WriteLine(vechile + " is start");
+        }
+        public void stopEngine()
+        {
+
+            Console.WriteLine(vechile + "is stop");
+        }
+    }
+
+    public class Car : Vechile
+    {
+        string name;
+        public Car(string name)
+        {
+            this.name = name;
+        }
+    }
+
+}
