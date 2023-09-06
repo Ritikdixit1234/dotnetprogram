07739a30c9756e0957c1d4032d565f54fec45618
Ritikdixit1234 <143371349+Ritikdixit1234@users.noreply.github.com>
2023-09-06 09:04:51 +0530

    Add files via upload

---
+++ ass2_q8.cs
@@ -0,0 +1,48 @@
+﻿//Question-8
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Cat c1 = new Cat();
+            d1.Sound("WOW WOW");
+            c1.Sound("MEOW MEOW");
+        }
+
+    }
+
+    public abstract class Animal
+    {
+        public abstract void Sound(string sound);
+    }
+    public class Dog : Animal
+    {
+        public override void Sound(string sound)
+        {
+            Console.WriteLine("Dog sound " + sound);
+            Console.ReadKey();
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void Sound(string sound)
+        {
+            Console.WriteLine("Cat Sound " + sound);
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
